72ef747c4119c1cd175a9925dca70220e5c09b6e
Natasha Ford <neford3991@gmail.com>
2017-08-11 15:59:22 +0100

    rename ListView to GodView to be more informative.

--- client/src/views/godView.js
+++ client/src/views/godView.js
@@ -1,19 +1,19 @@
-var ListView = function(listItems){
-  this.render(listItems);
+var GodView = function(god){
+  this.render(god);
 }
 
-ListView.prototype = {
-  render: function(listItems){
-    console.log(listItems);
-    listItems.forEach( function(item){
+GodView.prototype = {
+  render: function(god){
+    console.log(god);
+    god.forEach( function(god){
       var ul = document.getElementById('gods_list');
       var li = document.createElement('li');
-      li.innerText = item.name;
+      li.innerText = god.name;
       var image = document.createElement('img');
-      image.src = item.img;
+      image.src = god.img;
       image.style.height = '200px';
       var li1 = document.createElement('li');
-      li1.innerText = item.godOf;
+      li1.innerText = god.godOf;
       var li2 = document.createElement('li');
       li2.appendChild(image);     
       ul.appendChild(li);
@@ -23,4 +23,4 @@ ListView.prototype = {
   }
 }
 
-module.exports = ListView;
+module.exports = GodView;
