236728f1909cb6c8b07716545a6c8d24b1e07c49
Michael Robertson <michael_robertson_92@hotmail.co.uk>
2017-08-11 13:07:54 +0100

    changed location model

---
+++ client/db/egyptQuery.js
@@ -0,0 +1,9 @@
+var Mongo = requoire("mongodb").MongoClient;
+
+var EgyptQuery = function() {
+  this.url = "mongodb://localhost:27017/wonders_of_egypt";
+};
+
+EgyptQuery.prototype = {
+  
+}
--- client/src/models/location.js
+++ client/src/models/location.js
@@ -1,8 +1,11 @@
 var Location = function(options) {
   this.name = options.name;
-  this.lat = options.lat;
-  this.lng = options.lng;
-  this.link = options.link;
+  this.location = {
+    lat: options.lat,
+    lng: options.lng
+  }
+  this.wiki = options.wiki;
+  this.desc = options.desc;
 }
 
 module.exports = Location;
